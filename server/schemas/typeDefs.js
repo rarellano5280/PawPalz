263376645dd7a099278e6e6535ffdf2c9895ac80
Duran11 <antonio11duran@gmail.com>
2023-01-30 19:12:23 -0700

    removed postauthor from addpost mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -46,7 +46,7 @@ const typeDefs = gql`
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    addPost(postText: String!, postAuthor: String!): Post
+    addPost(postText: String!): Post
     addComment(postId: ID!, commentText: String!): Post
     removePost(postId: ID!): Post
     removeComment(postId: ID!, commentId: ID!): Post

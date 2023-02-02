76990aad4b5ac8b1652a70078a82ef1fd8248e78
Duran11 <antonio11duran@gmail.com>
2023-02-01 19:28:46 -0700

    added queries on client side

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,12 +1,73 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_THOUGHTS = gql`
-  query getThoughts {
-    thoughts {
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
       _id
-      thoughtText
-      thoughtAuthor
+      username
+      email
+      image
+      posts {
+        _id
+        postText
+        postAuthor
+        createdAt
+      }
+      friends {
+        userId
+        username
+        profilePic
+      }
+    }
+  }
+`;
+
+export const QUERY_POSTS = gql`
+  query getPosts {
+    posts {
+      _id
+      postText
+      postAuthor
       createdAt
+    }
+  }
+`;
+
+export const QUERY_SINGLE_POST = gql`
+  query getSinglePost($postId: ID!) {
+    post(postId: $postId) {
+      _id
+      postText
+      postAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        commentAuthor
+        createdAt
+      }
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      image
+      posts {
+        _id
+        postText
+        postAuthor
+        createdAt
+      }
+      friends {
+        userId
+        username
+        profilePic
+      }
     }
   }
 `;

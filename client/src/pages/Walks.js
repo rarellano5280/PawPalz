6d29bd76c19e6a966c125129d8780ae4a681f61c
Duran11 <antonio11duran@gmail.com>
2023-02-06 15:17:40 -0700

    added mapbox, need api key

--- client/src/pages/Walks.js
+++ client/src/pages/Walks.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
+// TO MAKE THE MAP APPEAR YOU MUST
+// ADD YOUR ACCESS TOKEN FROM
+// https://account.mapbox.com
+mapboxgl.accessToken = '<your access token here>';
+
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  style: 'mapbox://styles/mapbox/streets-v12', // style URL
+  center: [-74.5, 40], // starting position [lng, lat]
+  zoom: 9, // starting zoom
+});
+
+const geocoder = new MapboxGeocoder({
+  accessToken: mapboxgl.accessToken,
+  mapboxgl: mapboxgl,
+});
 
 const Walks = () => {
   return (
-    <div>Walks</div>
-  )
-}
+    <div>
+      <h4>{geocoder}</h4>
+      <h5>{map}</h5>
+    </div>
+  );
+};
 
-export default Walks
+export default Walks;

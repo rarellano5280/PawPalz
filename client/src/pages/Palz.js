da02a26b2d0dc11cfbcf327f00aaa6ccaf3b0367
Duran11 <antonio11duran@gmail.com>
2023-02-06 08:11:24 -0700

    adding search bar to palz

--- client/src/pages/Palz.js
+++ client/src/pages/Palz.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import { useQuery } from '@apollo/client';
+import { useMutation } from '@apollo/client';
+import { QUERY_ME } from '../utils/queries';
+import { QUERY_USER } from '../utils/queries';
+
+import Auth from '../utils/auth';
 
 const Palz = () => {
-  return (
-    <div>Palz</div>
-  )
-}
+  const { loading, data } = useQuery(QUERY_ME);
+  const friends = data?.me.friends || [];
 
-export default Palz
+  return (
+    <div>
+      {' '}
+      <form class="form-inline">
+        <input
+          class="form-control mr-sm-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        ></input>
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Palz;

3049f252ed690623f6a304cd07853cdf7f80f8dd
Robert <109295306+rarellano5280@users.noreply.github.com>
2023-02-07 21:30:16 -0700

    added some responsiveness to landing page

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="w-100 mt-auto gradient__text p-4">
+    <footer className="paw__footer w-100 mt-auto gradient__text p-4">
       <div className="container text-center mb-5">
         <h4>
           Designed by the Paw Palz Team
